f446e0c18577339f98f7db087edc2fef2a7b22ff
gulmezbugra <220254008@ogr.alanya.edu.tr>
2026-05-10 13:46:00 +0300

    fix(api): fix base URL and add missing fields for detail page

--- src/api/countriesApi.js
+++ src/api/countriesApi.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'https://restcountries.com/v3.1/all?fields=name,capital,currencies,flags',
+  baseURL: 'https://restcountries.com/v3.1',
   timeout: 10000,
 });
 
 export const countriesApi = {
-  getAll: () => client.get('/all'),
+  getAll: () => client.get('/all?fields=name,capital,currencies,flags,cca3,region,subregion,population,area,languages,borders,coatOfArms,maps,continents,tld,timezones'),
 };
